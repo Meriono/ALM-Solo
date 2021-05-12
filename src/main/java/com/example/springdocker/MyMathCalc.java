a2544ae6708adddbc1a7a00ae7175321391d8725
Hanna Edlund <72083540+Meriono@users.noreply.github.com>
2021-05-12 11:43:34 +0200

    Mathcalc (#1)
    * added math class
    
    * added methods, add multiply and divide

---
+++ src/main/java/com/example/springdocker/MyMathCalc.java
@@ -0,0 +1,22 @@
+package com.example.springdocker;
+
+/**
+ * Created by Hanna Edlund
+ * Date: 2021-05-12
+ * Time: 10:58
+ * Project: spring-docker-demo
+ */
+public class MyMathCalc {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+}
