4847a450cc8a023977696b74f187daddb859a272
Hanna Edlund <hanna@edlunds.biz>
2021-05-14 09:40:21 +0200

    added test methods

--- src/test/java/com/example/springdocker/MyMathCalcTest.java
+++ src/test/java/com/example/springdocker/MyMathCalcTest.java
@@ -1,5 +1,9 @@
 package com.example.springdocker;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * Created by Hanna Edlund
  * Date: 2021-05-14
@@ -7,4 +11,34 @@ package com.example.springdocker;
  * Project: spring-docker-demo
  */
 public class MyMathCalcTest {
+    MyMathCalc math = new MyMathCalc();
+
+    @Test
+    public void testAddMethod(){
+        int expected = 5;
+        int actual = math.add(2,3);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testMultipyMethod(){
+        int expected = 6;
+        int actual = math.multiply(2,3);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testDivideMethod(){
+        int expected = 5;
+        double actual = math.divide(10,2);
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testDivideThrowsZero(){
+        assertThrows(ArithmeticException.class,() -> math.divide(2,0));
+    }
 }
